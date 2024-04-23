d0c264e8205e626f36d1ddfe0d7972c7fbdfc4d4
andreasthealien <headmaster2407@gmail.com>
2024-04-23 19:49:46 +0200

    made tord functions!

--- functions/db.js
+++ functions/db.js
@@ -1,2 +1,38 @@
 import mongoose from "mongoose";
 
+//
+const dareSchema = new mongoose.Schema({
+    dare: {
+        type: String,
+        required: true
+    },
+    cond: {
+        type: Boolean,
+        required: true
+    },
+    cuff: {
+        type: Boolean,
+        required: true
+    },
+    blin: {
+        type: Boolean,
+        required: true
+    },
+    nothing: {
+        type: Boolean,
+        required: true
+    }
+}); //dare: String, cond: Boolean, cuff: Boolean, blin: Boolean, nothing: Boolean
+
+const truthSchema = new mongoose.Schema({
+    truth: {
+        type: String,
+        required: true
+    }
+}); //truth: String
+
+
+const Dare = mongoose.Model("Dare", dareSchema);
+const Truth = mongoose.Model("Truth", truthSchema);
+
+export { Dare, Truth };
--- functions/db_functions.js
+++ functions/db_functions.js
@@ -1,10 +1,121 @@
 import mongoose from "mongoose";
+import { Dare, Truth } from "./db.js";
 
-function add() {}
-function edit() {}
-function remove() {}
+async function addDare(dareStr, condBoo, cuffBoo, blinBoo, nothingBoo) {
+    const newDare = new Dare({
+        dare: dareStr,
+        cond: condBoo,
+        cuff: cuffBoo,
+        blin: blinBoo,
+        nothing: nothingBoo
+    });
 
-function truth() {}
-function dare() {}
-function dareFilter() {}
-function random() {}
+    try {
+        await newDare.save();
+        return true;
+    } catch (err) {
+        return false;
+    };
+};
+async function editDare(id, updateObject) {
+    try {
+        await Dare.findOneAndUpdate({ _id: id }, updateObject);
+        return true;
+    } catch (err) {
+        return false;
+    };
+};
+async function removeDare(id) {
+    try {
+        await Dare.remove({ _id: id });
+        return true;
+    } catch (err) {
+        return false;
+    };
+};
+
+async function addTruth(truthStr) {
+    const newTruth = new Truth({
+        truth: truthStr
+    });
+
+    try {
+        await newTruth.save();
+        return true;
+    } catch (err) {
+        return false;
+    };
+};
+async function editTruth(id, updateObject) {
+    try {
+        await Truth.findOneAndUpdate({ _id: id }, updateObject);
+        return true;
+    } catch (err) {
+        return false;
+    };
+};
+async function removeTruth(id) {
+    try {
+        await Truth.remove({ _id: id });
+        return true;
+    } catch (err) {
+        return false;
+    };
+};
+
+async function truth() {
+    try {
+        const truthArr = await Truth.find();
+        const truthObj = truthArr[Math.floor(Math.random()*truthArr.length)]
+
+        return truthObj
+    } catch (err) {
+        console.log(err.error);
+        return false;
+    };
+};
+async function dare() {
+    try {
+        const dareArr = await Truth.find();
+        const dareObj = dareArr[Math.floor(Math.random()*dareArr.length)]
+
+        return dareObj
+    } catch (err) {
+        console.log(err.error);
+        return false;
+    };
+};
+async function dareFilter(req) {
+    try {
+        const dareArr1 = await Truth.find();
+
+        const dareArr2 = dareArr1.filter((dare) => dare.nothing === req.body.nothing && dare.blin === req.body.blin && dare.cuff === req.body.cuff && dare.cond === req.body.cond);
+
+        const dareObj = dareArr2[Math.floor(Math.random()*dareArr2.length)]
+
+        return dareObj;
+    } catch (err) {
+        console.log(err.error);
+        return false;
+    };
+};
+async function random() {
+    try {
+        const randInt = Math.floor(Math.floor()*2);
+        if (randInt === 0){
+            const truthArr = await Truth.find();
+            const truthObj = truthArr[Math.floor(Math.random()*truthArr.length)]
+
+            return truthObj
+        } else if (randInt === 1){
+            const dareArr = await Truth.find();
+            const dareObj = dareArr[Math.floor(Math.random()*dareArr.length)]
+
+            return dareObj
+        };
+    } catch (err) {
+        console.log(err.error);
+        return false;
+    };
+};
+
